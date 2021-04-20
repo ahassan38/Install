e2fc3521aed46eb831d2e5a0ecd63315d5a68553
Hass <hassan.a38240@gmail.com>
2021-04-20 11:11:25 +0200

    Add images

--- resources/views/pages/Mon Appli.blade.php
+++ resources/views/pages/Mon Appli.blade.php
@@ -4,9 +4,12 @@
 
 
 @section('content')
+
+        <img src="/images/FR.png" alt="French flag">
+        
         <h1>Best World Time !<h1>
 
-            <p>Il est actuellement {{date('H:i')}}</p>
+        <p>Il est actuellement {{date('H:i')}}</p>
 
 @endsection
 
--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,9 @@
 @section('title','A propos de |' . config('app.name'))
 
 @section('content')
-        
+
+        <img src="/images/sato.gif" alt="manga avatar">
+
         <p>élaboré &hearts; par mes soins!</p>
 
         <p><a href="{{route('home')}}">Revenir à la page d'accueil></a></p>
